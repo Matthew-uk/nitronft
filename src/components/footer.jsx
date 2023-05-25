d0e55bdbf0828af587bc135da06387e1b19e6a4e
Matthew-uk <esaduviedede@gmail.com>
2023-05-25 12:28:28 +0100

    Just changed link

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -1,9 +1,13 @@
 // import Heart from './../img/heart-svgrepo-com.svg';
 
-export default function Footer(){
-  return(
+export default function Footer() {
+  return (
     <div className="footer">
-      <p>Made with <span className="hearts">&hearts;</span> by <a href="https://www.google.com">Matthew Ukarionisofien</a>, design by <a href="#jbjdbj">Samuel Jarvis</a> </p>
+      <p>
+        Made with <span className="hearts">&hearts;</span> by{" "}
+        <a href="https://github.com/Matthew-uk">Matthew Ukarionisofien</a>,
+        design by <a href="#jbjdbj">Samuel Jarvis</a>{" "}
+      </p>
     </div>
   );
 }
